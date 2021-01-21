164fe5908e127ddea3467167d6a8f99b6850df28
Robert Szwed <robertszwed98@gmail.com>
2021-01-21 15:44:05 +0100

    dodanie bazy danych dataBase version 0

--- Pages/RaseChoice.xaml.cs
+++ Pages/RaseChoice.xaml.cs
@@ -12,20 +12,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+
+using Dapper;
 
 namespace WHeditor
 {
     /// <summary>
     /// Logika interakcji dla klasy RaseChoice.xaml
     /// </summary>
+    /// 
+
+
+
+
     public partial class RaseChoice : Page
     {
 
-        
+        DBConnectionDataContext dbc = new DBConnectionDataContext(Properties.Settings.Default.WHPE_dbConnectionString);
         public RaseChoice()
         {
             InitializeComponent();
-        }
+        }  
 
 
 
@@ -33,12 +44,39 @@ namespace WHeditor
 
         private void RaceChoiceButtonWariorIcon_Click (object sender, RoutedEventArgs e)
         {
+            //----------------
+
+            string connectionString;
+            SqlConnection cnn;
+
+            connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\WHPE_db.mdf;Integrated Security=True";
+
+            cnn = new SqlConnection(connectionString);
+
+            cnn.Open();
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = "";
+
+            sql = "Select RaseName From Rase Where id = '1'";
+
+            command = new SqlCommand(sql, cnn);
+
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                Output = Output + dataReader.GetValue(0) ;
+            }
+
+
+
+
+            //----------------
+
+            RaseChoiceTextBlockRaseDescription.Text = Output;
             RaseChoiceImageRaseImg.Source = new BitmapImage(new Uri("../Images/PageRaseChoice/WariorImg.png", UriKind.Relative));
-            RaseChoiceTextBlockRaseDescription.Text = "SimpleTextHuman\n"+
-                "111LoriumMadafakiumFakapium...111LoriumMadafakiumFakapium..." +
-                "111LoriumMadafakiumFakapium...111LoriumMadafakiumFakapium...111LoriumMadafakiumFakapium...111LoriumMadafakiumFakapium...";
-
-
         }
         private void RaceChoiceButtonElfIcon_Click(object sender, RoutedEventArgs e)
         {
