6a4c5d92563d76a31d595244cd30f6af411adce6
Robert Szwed <rs052973@student.ath.edu.pl>
2021-02-15 14:00:05 +0100

    dodanie klasaProfessionRoll

---
+++ Func/ProfessionRollValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WHeditor
+{
+    public static class ProfessionRollValue
+    {
+     
+        public static int RollProfessionAndGetID()
+        {
+            int ID;
+
+
+
+
+            return ID;
+        }
+
+
+    }
+}
